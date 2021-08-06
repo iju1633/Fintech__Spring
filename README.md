412730bde4fecb030ce6cadef85736da41288b85
Garfield <iju1633@gmail.com>
2021-08-06 23:56:04 +0900

    준비 완료!

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # Fintech-SpringBoot
-
-## 핀테크 지원센터 Pre-인턴십 금융IT 실습
+springboot-tutorial-init
--- src/main/java/com/example/finedu/entity/Hello.java
+++
@@ -1,8 +0,0 @@
-package com.example.finedu.entity;
-
-import lombok.Data;
-
-@Data
-public class Hello {
-    final String name;
-}
